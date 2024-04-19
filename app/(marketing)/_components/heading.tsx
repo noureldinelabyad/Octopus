0a15b3daedff9e023c2bd7c3c3429e2858fe1276
Noureldin Elabyad <Noureldin.Elabyad@thinfabrics.com>
2024-04-20 01:28:08 +0200

    landing page

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import {Button} from "@/components/ui/button"
+import { ArrowRight } from "lucide-react";
+
+export const Heading = () => {
+    return (
+        <div className="max-w-3xl space-y-4 p-6" >
+            <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold">
+                Your 8 Arms Planer, Welcom to <span
+                 className="underline" style={{ color: '#BB007F' }} >Octopus</span>
+            </h1>
+            <h3  className="text-base sm:text-l md:text-xl font-medium">
+                Octopus is the pergect workspace <br/>
+                where you connet all your worlds!
+            </h3>
+            <Button>
+                Creat your New world
+                <ArrowRight className="h-4 w-4 ml-2"/>
+            </Button>
+        </div>
+    )
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import {Heading}  from "./_components/heading";
+import { Button } from "@/components/ui/button";
+import { Heroes } from "./_components/heroes";
+import { Footer } from "./_components/footer";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col">
+      {/* this div gonna take mejorty of the screen, md: for medum devices  */}
+      <div className="flex flex-col items-center justify-center
+       md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">   
+        <Heading/>
+        <Heroes/>
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default MarketingPage;
